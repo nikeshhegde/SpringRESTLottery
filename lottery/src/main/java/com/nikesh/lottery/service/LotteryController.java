37cdb78567dbc85d23f6446586c06e1447c51aca
Nikesh <nhegde05@gmail.com>
2020-08-09 23:15:20 +0100

    Updated LotteryController with comments

--- lottery/src/main/java/com/nikesh/lottery/service/LotteryController.java
+++ lottery/src/main/java/com/nikesh/lottery/service/LotteryController.java
@@ -16,20 +16,45 @@ import com.nikesh.lottery.util.TicketResult;
 @RestController
 @RequestMapping("/lottery")
 public interface LotteryController {
-
+	
+	/**
+	 * Create a ticket
+	 * @param lineNumbers - numbers of line to be created in the ticket
+	 * @return Result with the generated ticket with no of lines
+	 */
 	@PostMapping("/ticket/create")
 	LotteryTicket createLotteryTicket(@RequestParam(value = "lineNumbers") Integer lineNumbers);
-
+	
+	/**
+	 * Return list of tickets
+	 * @return List of all tickets
+	 */
 	@GetMapping("/ticket/read")
 	List<LotteryTicket> getAllTicket();
 
+	/**
+	 * Get individual ticket
+	 * @param ticketId - the ticket details for the ticketId 
+	 * @return 
+	 */
 	@GetMapping("/ticket")
 	LotteryTicket getSingleTicket(@RequestParam(value = "ticketId") Long ticketId);
 
+	/**
+	 * Amend ticket lines
+	 * @param lineNumbers - numbers of line to be created in the ticket
+	 * @param ticketId - the ticket id for which lines are to be amended
+	 * @return the result after we amend the lines
+	 */
 	@PutMapping("/ticket/update")
 	LotteryTicket amendTicketLine(@RequestParam(value = "lineNumbers") Integer lineNumbers,
 			@RequestParam(value = "ticketId") Long ticketId);
 
+	/**
+	 * Retrieve status of ticket
+	 * @param ticketId - ticketId for which the status is to be returned
+	 * @return ticket details
+	 */
 	@PutMapping("/ticket/status")
 	TicketResult retrieveTicketStatus(@RequestParam(value = "ticketId") Long ticketId);
 
